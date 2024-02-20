d5d8d3bf971cde362472ccc65f3b5c39e0b5a9c7
ZenitsuCoxinha <137904076+ZenitsuCoxinha@users.noreply.github.com>
2024-02-20 11:21:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -28,8 +28,16 @@ function speak() {
   var synth = window.speechSynthesis;  
   speakData1 = "A primeira previsão é " + prediction1;
   speakData2 = "E a segunda previsão é " + prediction2;
-  var utterThis = new SpeechSynthesisUtterance(speakDate1 + speakDate2);
+  var utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
   synth.speak(utterThis);
+}
+
+function check()
+{
+
+img = document.getElementById('captured_image');
+classifier.classify(img, gotResult );
+
 }
 
 function gotResult(error, results) {
